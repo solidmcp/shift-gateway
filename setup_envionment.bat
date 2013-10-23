ea33b916a8a95b57f213fbb07755cd14691dea01
Piaoger <piaoger@yahoo.com>
2013-10-23 23:43:55 +0800

    update CEF to 3.1547.1412[3]

--- setup_envionment.bat
+++ setup_envionment.bat
@@ -1,3 +1,3 @@
 
-"tools/win32/7zip/7z.exe" x -osrc deps/cef/cef_binary_3.1180.823_debug_x86.7z
-"tools/win32/7zip/7z.exe" x -osrc deps/cef/cef_binary_3.1180.823_release_x86.7z
+"tools/win32/7zip/7z.exe" x -osrc deps/cef/cef_binary_3.1547.1412_debug_x86.7z
+"tools/win32/7zip/7z.exe" x -osrc deps/cef/cef_binary_3.1547.1412_release_x86.7z
